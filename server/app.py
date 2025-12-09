8581ba1ee3cc81b62581b04fdc3a11c23d3830cf
Artem Raskin <artemraskin@Artems-MacBook-Pro.local>
2025-12-09 14:04:15 -0800

    Add GET/restaurants route to return list of restaurants

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db'
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+# app.json.compact = False
 
 migrate = Migrate(app, db)
 
@@ -20,9 +20,24 @@ db.init_app(app)
 api = Api(app)
 
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+class Index(Resource):
+    def get(self):
+        return {"message": "Code challenge"}, 200
+    
+
+class Restaurants(Resource):
+    def get(self):
+        restaurants = [
+            r.to_dict(only=("address", "name", "id"))
+            for r in Restaurant.query.all()
+            ]
+
+        return restaurants, 200
+
+    
+api.add_resource(Index, "/")
+api.add_resource(Restaurants, "/restaurants")
+
 
 
 if __name__ == "__main__":
