59860985243b3c980a861c2a9fa9bc29c5bf8fc8
Artem Raskin <artemraskin@Artems-MacBook-Pro.local>
2025-12-09 10:36:39 -0800

    Add model relationships between Restaurant, Pizza, and RestaurantPizza

--- server/models.py
+++ server/models.py
@@ -12,7 +12,7 @@ metadata = MetaData(
 
 db = SQLAlchemy(metadata=metadata)
 
-
+# --- has many ---
 class Restaurant(db.Model, SerializerMixin):
     __tablename__ = "restaurants"
 
@@ -21,13 +21,17 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship(
+        "RestaurantPizza", 
+        back_populates="restaurant",
+        cascade="all, delete-orphan"
+        )
     # add serialization rules
-
+    
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
-
+# --- has many ---
 class Pizza(db.Model, SerializerMixin):
     __tablename__ = "pizzas"
 
@@ -36,21 +40,24 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza")
     # add serialization rules
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
-
+# --- belongs to Pizza and Restaurant ---
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = "restaurant_pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
 
     # add relationships
-
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
     # add serialization rules
 
     # add validation
